2196e558a95072f135c0e861365cf16c1848894c
Pelanyo Kamara <pelanyokamara@gmail.com>
2018-08-04 20:39:33 +0100

    Create Settings.lua

---
+++ Repo/Client/Modules/Settings.lua
@@ -0,0 +1,15 @@
+local SB = {
+	User = {
+		Key         = "DefaultKey",		
+		PacketCount = 0,
+		SelfPacketCount = 0,
+	},
+	
+	Server = {
+		ServerKey       = "03k705x6n7U4zC2qf4Wk917AlNi4mBT7",
+		Rot             = {28, 10, 20},
+	}
+}
+
+
+return SB
